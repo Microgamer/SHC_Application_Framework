9d981cfa1ced0e0ce2e1ba4a4cc74515fd963920
Oliver <webmaster@rpi-controlcenter.de>
2015-12-30 09:14:30 +0100

    add BasicElement

---
+++ SHC_Core/src/net/kleditzsch/shcCore/Core/BasicElement.java
@@ -0,0 +1,91 @@
+package net.kleditzsch.shcCore.Core;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
+
+/**
+ * Basiselement aller SHC Datenobjekte
+ *
+ * @author Oliver Kleditzsch
+ * @copyright Copyright (c) 2015, Oliver Kleditzsch
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+public abstract class BasicElement {
+
+    /**
+     * Eindeutige Identifizierung
+     */
+    private String hash = "";
+
+    /**
+     * Name des Elements
+     */
+    private String name = "";
+
+    /**
+     * gibt die eindeutige Identifizierung des Elements zurück
+     *
+     * @return Hash
+     */
+    public String getHash() {
+        return hash;
+    }
+
+    /**
+     * setzt die die eindeutige Identifizierung des Elements
+     *
+     * @param hash eindeutige Identifizierung
+     */
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    /**
+     * erzeugt einen neuen Hash zur eindeutigen Identifizierung
+     *
+     * @return neue eindeutige Identifizierung
+     */
+    public static Optional<String> createHash() {
+
+        Optional<String> response = Optional.empty();
+        long nanoTime = System.nanoTime();
+
+        MessageDigest md = null;
+        try {
+
+            md = MessageDigest.getInstance("SHA1");
+
+            md.reset();
+            byte[] buffer = Long.toString(nanoTime).getBytes();
+            md.update(buffer);
+            byte[] digest = md.digest();
+
+            String hexStr = "";
+            for (int i = 0; i < digest.length; i++) {
+                hexStr +=  Integer.toString( ( digest[i] & 0xff ) + 0x100, 16).substring( 1 );
+            }
+            response = Optional.of(hexStr);
+        } catch (NoSuchAlgorithmException e) {}
+
+        return response;
+    }
+
+    /**
+     * gibt den Namen des Elements zurück
+     *
+     * @return Name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * setzt den Namen des Elements
+     *
+     * @param name Name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
