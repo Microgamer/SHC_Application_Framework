484af9c5c9d90690b8f4ce9cf1f43c2657686a8e
Oliver <webmaster@rpi-controlcenter.de>
2016-01-02 07:27:46 +0100

    remove überflüssige Implementierung

--- SHC_Core/src/net/kleditzsch/shcCore/Room/Elemnets/Readables/AbstractInput.java
+++ SHC_Core/src/net/kleditzsch/shcCore/Room/Elemnets/Readables/AbstractInput.java
@@ -1,7 +1,6 @@
 package net.kleditzsch.shcCore.Room.Elemnets.Readables;
 
 import net.kleditzsch.shcCore.Room.Elemnets.Abstract.AbstractReadable;
-import net.kleditzsch.shcCore.Room.Elemnets.Interface.Readable;
 import net.kleditzsch.shcCore.SwitchServer.Interface.SwitchServer;
 
 /**
@@ -11,7 +10,7 @@ import net.kleditzsch.shcCore.SwitchServer.Interface.SwitchServer;
  * @copyright Copyright (c) 2016, Oliver Kleditzsch
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  */
-public abstract class AbstractInput extends AbstractReadable implements Readable {
+public abstract class AbstractInput extends AbstractReadable {
 
     /**
      * Schaltserver
