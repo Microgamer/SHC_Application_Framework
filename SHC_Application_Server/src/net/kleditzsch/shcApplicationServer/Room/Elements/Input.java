7b10d80d35f9c7de791fd007677f74c73e9380b6
Oliver <webmaster@rpi-controlcenter.de>
2016-01-03 07:51:57 +0100

    Add Input

---
+++ SHC_Application_Server/src/net/kleditzsch/shcApplicationServer/Room/Elements/Input.java
@@ -0,0 +1,25 @@
+package net.kleditzsch.shcApplicationServer.Room.Elements;
+
+import net.kleditzsch.shcCore.Room.Elemnets.Elements.AbstractInput;
+
+/**
+ * Eingang
+ *
+ * @author Oliver Kleditzsch
+ * @copyright Copyright (c) 2016, Oliver Kleditzsch
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+public class Input extends AbstractInput {
+
+    /**
+     * liest den Status des Elements
+     *
+     * @return Status
+     */
+    @Override
+    public int readState() {
+
+        //TODO implementieren
+        return 0;
+    }
+}
