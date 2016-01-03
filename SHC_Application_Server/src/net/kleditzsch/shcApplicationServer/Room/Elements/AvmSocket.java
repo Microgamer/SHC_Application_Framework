1fd292027cf7a6516746aa83b3805a47ceea9d4d
Oliver <webmaster@rpi-controlcenter.de>
2016-01-03 07:51:18 +0100

    Add AVM Steckdose

---
+++ SHC_Application_Server/src/net/kleditzsch/shcApplicationServer/Room/Elements/AvmSocket.java
@@ -0,0 +1,37 @@
+package net.kleditzsch.shcApplicationServer.Room.Elements;
+
+import net.kleditzsch.shcCore.Room.Elemnets.Elements.AbstractAvmSocket;
+
+/**
+ * Kommentar
+ *
+ * @author Oliver Kleditzsch
+ * @copyright Copyright (c) 2016, Oliver Kleditzsch
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+public class AvmSocket extends AbstractAvmSocket {
+
+    /**
+     * Aktion die bei Betätigung des "an" Buttons ausgeführt wird
+     */
+    @Override
+    public void triggerOn() {
+        super.triggerOn();
+    }
+
+    /**
+     * Aktion die bei Betätigung des "aus" Buttons ausgeführt wird
+     */
+    @Override
+    public void triggerOff() {
+        super.triggerOff();
+    }
+
+    /**
+     * liest die Sensordaten und den Status der Steckdose
+     */
+    public void readData() {
+
+
+    }
+}
