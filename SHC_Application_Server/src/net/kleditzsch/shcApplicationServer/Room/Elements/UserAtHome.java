4cef005ef855ddd56382807ab66c686b460153ff
Oliver <webmaster@rpi-controlcenter.de>
2016-01-03 07:52:21 +0100

    Add benutzer zu Hause

---
+++ SHC_Application_Server/src/net/kleditzsch/shcApplicationServer/Room/Elements/UserAtHome.java
@@ -0,0 +1,43 @@
+package net.kleditzsch.shcApplicationServer.Room.Elements;
+
+import net.kleditzsch.shcCore.Room.Elemnets.Elements.AbstractUserAtHome;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+/**
+ * Benutzer zu Hause
+ *
+ * @author Oliver Kleditzsch
+ * @copyright Copyright (c) 2016, Oliver Kleditzsch
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+public class UserAtHome extends AbstractUserAtHome {
+
+    /**
+     * liest den Status des Elements
+     * (die Ausführung kann bis zu 500ms dauern, diese Methode sollte Asynchron verwendet werden)
+     *
+     * @return Status
+     */
+    @Override
+    public int readState() {
+
+        try {
+
+            InetAddress inetAddress = InetAddress.getByName(ipAddress);
+            if(inetAddress.isReachable(500)) {
+
+                state = ONLINE;
+            } else {
+
+                state = OFFLINE;
+            }
+
+        } catch (IOException e) {
+
+            state = OFFLINE;
+        }
+        return state;
+    }
+}
