e5fe50001dfa1ceeb9e938253c24dea6bd7a5606
Oliver <webmaster@rpi-controlcenter.de>
2016-01-06 06:10:01 +0100

    Add Hygrometer und Regensensor

---
+++ SHC_Application_Server/src/net/kleditzsch/shcApplicationServer/Room/Elements/Hygrometer.java
@@ -0,0 +1,15 @@
+package net.kleditzsch.shcApplicationServer.Room.Elements;
+
+import net.kleditzsch.shcCore.Room.Elements.Elements.AbstractHygrometer;
+
+/**
+ * Feuchtigkeits Sensor
+ *
+ * @author Oliver Kleditzsch
+ * @copyright Copyright (c) 2016, Oliver Kleditzsch
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+public class Hygrometer extends AbstractHygrometer {
+
+
+}
