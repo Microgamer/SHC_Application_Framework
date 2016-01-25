86d013da2cbb8945f2c93c7aecaa74d35176bc89
Oliver <webmaster@rpi-controlcenter.de>
2016-01-25 05:42:36 +0100

    add Support Klasse für Edimax SP-1101W Smartplug

--- JavaTools/src/main/java/net/kleditzsch/Edimax/SmartPug/SP1101.java
+++ JavaTools/src/main/java/net/kleditzsch/Edimax/SmartPug/SP1101.java
@@ -1,14 +1,14 @@
 package net.kleditzsch.Edimax.SmartPug;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.*;
+import java.net.*;
 
 /**
  * Edimax SmartPlug SP-1101
+ *
+ * @author Oliver Kleditzsch
+ * @copyright Copyright (c) 2015, Oliver Kleditzsch
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  */
 public class SP1101 {
 
@@ -37,6 +37,9 @@ public class SP1101 {
      */
     private String password;
 
+    /**
+     * @param ip IP Adresse
+     */
     public SP1101(String ip){
 
         this.ip = ip;
@@ -44,6 +47,10 @@ public class SP1101 {
         this.password = "1234";
     }
 
+    /**
+     * @param ip IP Adresse
+     * @param password Passwort
+     */
     public SP1101(String ip, String password) {
 
         this.ip = ip;
@@ -51,6 +58,11 @@ public class SP1101 {
         this.password = password;
     }
 
+    /**
+     * @param ip IP Adresse
+     * @param user Benutzername
+     * @param password Passwort
+     */
     public SP1101(String ip, String user, String password) {
 
         this.ip = ip;
@@ -58,31 +70,61 @@ public class SP1101 {
         this.password = password;
     }
 
+    /**
+     * gibt die IP Adresse zurück
+     *
+     * @return IP Adresse
+     */
     public String getIp() {
 
         return ip;
     }
 
+    /**
+     * setzt die IP Adresse
+     *
+     * @param ip IP Adresse
+     */
     public void setIp(String ip) {
 
         this.ip = ip;
     }
 
+    /**
+     * gibt den Benutzernamen zurück
+     *
+     * @return Benutzernamen
+     */
     public String getUser() {
 
         return user;
     }
 
+    /**
+     * setzt den Benutzernamen
+     *
+     * @param user Benutzernamen
+     */
     public void setUser(String user) {
 
         this.user = user;
     }
 
+    /**
+     * gibt das Passwort zurück
+     *
+     * @return Passwort
+     */
     public String getPassword() {
 
         return password;
     }
 
+    /**
+     * setzt das Passwort
+     *
+     * @param password Passwort
+     */
     public void setPassword(String password) {
 
         this.password = password;
@@ -91,17 +133,28 @@ public class SP1101 {
     /**
      * prüft ob die Steckdose erreichbar ist
      *
-     * @return
+     * @return true wenn erreichbar
      */
     public boolean isPresent() {
 
+        try {
+
+            InetAddress ip = InetAddress.getByName(getIp());
+            if(ip.isReachable(1000)) {
+
+                return true;
+            }
+            return false;
+        } catch (IOException ex) {}
         return false;
     }
 
     /**
      * sendet einen Einschaltbefehl
+     *
+     * @return true bei Erfolg
      */
-    public void switchOn() {
+    public boolean switchOn() throws IOException {
 
         String request = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
                 "                        <SMARTPLUG id=\"edimax\">\n" +
@@ -110,62 +163,108 @@ public class SP1101 {
                 "                            </CMD>\n" +
                 "                        </SMARTPLUG>";
 
-        try {
-            String response = sendHttpCommand(request);
-            System.out.println(response);
-        } catch (IOException e) {
-            e.printStackTrace();
+        String response = sendHttpCommand(request);
+        if(response.contains("<CMD id=\"setup\">OK</CMD>")) {
+
+            return true;
         }
+        return false;
     }
 
     /**
      * sendet einen Ausschaltbefehl
+     *
+     * @return true bei Erfolg
      */
-    public void switchOff() {
+    public boolean switchOff() throws IOException {
 
+        String request = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                "                        <SMARTPLUG id=\"edimax\">\n" +
+                "                            <CMD id=\"setup\">\n" +
+                "                                <Device.System.Power.State>OFF</Device.System.Power.State>\n" +
+                "                            </CMD>\n" +
+                "                        </SMARTPLUG>";
 
+        String response = sendHttpCommand(request);
+        if(response.contains("<CMD id=\"setup\">OK</CMD>")) {
+
+            return true;
+        }
+        return false;
     }
 
     /**
      * fragt den aktuellen Status der Steckodse ab
+     *
+     * @return STATE_ON oder STATE_OFF
      */
-    public boolean readState() {
+    public int readState() throws IOException {
 
-        return false;
+        String request = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                "                        <SMARTPLUG id=\"edimax\">\n" +
+                "                            <CMD id=\"get\">\n" +
+                "                                <Device.System.Power.State></Device.System.Power.State>\n" +
+                "                            </CMD>\n" +
+                "                        </SMARTPLUG>";
+
+        String response = sendHttpCommand(request);
+        if(response.contains("<Device.System.Power.State>ON</Device.System.Power.State>")) {
+
+            return STATE_ON;
+        }
+        return STATE_OFF;
     }
 
     /**
      * sendet einen Befehl per HTTP an die Steckdose
      *
-     * @param content
-     * @return
+     * @param content XML ANfrage
+     * @return XML Antwort
      */
     private String sendHttpCommand(String content) throws IOException {
 
-        URL url = new URL("http://" + getUser() + ":" + getPassword() + "@" + getIp() + ":10000/smartplug.cgi");
+        final String user = getUser();
+        final String password = getPassword();
+
+        //Authentifizierung
+        Authenticator.setDefault(new Authenticator() {
+
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, password.toCharArray());
+            }
+        });
+
+        URL url = new URL("http://" + getIp() + ":10000/smartplug.cgi");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
         //Anfrage vorbereiten
-        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
         connection.setRequestProperty("User-Agent", "Java Tools");
         connection.setRequestProperty("Content-Type", "text/xml");
         connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-        connection.setReadTimeout(500);
+        connection.setRequestProperty("charset", "utf-8");
+        connection.setRequestProperty("Content-Length", Integer.toString(content.toCharArray().length));
+        connection.setUseCaches(false);
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        connection.setConnectTimeout(1000);
+        connection.setReadTimeout(3000);
 
         //Daten senden
-        OutputStream outputStream = connection.getOutputStream();
-        outputStream.write(content.getBytes());
-        //BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-        //bufferedWriter.write(content);
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        wr.writeBytes(content);
+        wr.flush ();
+        wr.close ();
 
         //Antwort auslesen
+        BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String line;
         StringBuffer response = new StringBuffer();
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        while((line = in.readLine()) != null) {
-
+        while((line = rd.readLine()) != null) {
             response.append(line);
         }
+        rd.close();
         return response.toString();
     }
 }
