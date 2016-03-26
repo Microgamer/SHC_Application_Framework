5430fc642e941a422794f005a5c2c16352018aa1
Oliver <webmaster@rpi-controlcenter.de>
2016-03-26 13:58:31 +0100

    Add Icon Verwaltung

--- SHC_Application_Server/src/main/java/net/kleditzsch/shcApplicationServer/Icon/Icon.java
+++ SHC_Application_Server/src/main/java/net/kleditzsch/shcApplicationServer/Icon/Icon.java
@@ -29,6 +29,11 @@ public class Icon {
     protected Path path;
 
     /**
+     * gibt an ob die Datei in einem Jar Archiv liegt
+     */
+    protected boolean jarFile = false;
+
+    /**
      * gibt die Liste mit den verfügbaren größen zurück
      *
      * @return verfügbare größen
@@ -71,5 +76,65 @@ public class Icon {
      */
     public void setPath(Path path) {
         this.path = path;
+    }
+
+    /**
+     * gibt an ob die Icon Datei in einem Jar Archiv liegt
+     *
+     * @return Jar Archiv
+     */
+    public boolean isJarFile() {
+        return jarFile;
+    }
+
+    /**
+     * markiert die Date als Jar Archiv Datei
+     *
+     * @param jarFile Jar Archiv
+     */
+    public void setJarFile(boolean jarFile) {
+        this.jarFile = jarFile;
+    }
+
+    /**
+     * gibt die kleiste verfügbare Icon größe zurück
+     *
+     * @return Icon größe
+     */
+    public int getMinSize() {
+
+        int minSize = 0;
+        for(int size : availableSize) {
+
+            if(minSize == 0 || size < minSize) {
+
+                minSize = size;
+            }
+        }
+        return minSize;
+    }
+
+    /**
+     * gibt die größte verfügbare Icon größe zurück
+     *
+     * @return Icon größe
+     */
+    public int getMaxSize() {
+
+        int maxSize = 0;
+        for(int size : availableSize) {
+
+            if(maxSize == 0 || size > maxSize) {
+
+                maxSize = size;
+            }
+        }
+        return maxSize;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Icon [name=" + getName() + "; file=" + getPath().toAbsolutePath().toString() + "]";
     }
 }
--- SHC_Application_Server/src/main/java/net/kleditzsch/shcApplicationServer/Icon/IconEditor.java
+++ SHC_Application_Server/src/main/java/net/kleditzsch/shcApplicationServer/Icon/IconEditor.java
@@ -5,7 +5,10 @@ import net.kleditzsch.shcCore.Util.LoggerUtil;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,42 +28,154 @@ public class IconEditor {
      */
     protected List<Icon> iconList = new ArrayList<>();
 
+    /**
+     * liest die Systemicons
+     */
     public void loadSystemIcons() {
 
-        Path icons = Paths.get(IconEditor.class.getProtectionDomain().getCodeSource().getLocation().getPath());
-        if(icons.getFileName().endsWith(".jar")) {
+        Path iconsDirectory = Paths.get(IconEditor.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        if(iconsDirectory.getFileName().toString().toLowerCase().endsWith(".jar")) {
 
             //Jar Datei
-            System.out.println("Jar Icons");
+            readIconsFromJarFile(iconsDirectory);
         } else {
 
-            //Ordner
-            System.out.println("Directory Icons");
-            icons = Paths.get(IconEditor.class.getClassLoader().getResource("icons").getPath());
-            System.out.println(icons.toAbsolutePath());
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream(icons)) {
-                for (Path file: stream) {
+            //Ordner Entwicklungsmodus
+            readIconsFromDirectory(Paths.get(IconEditor.class.getClassLoader().getResource("icons").getPath()));
+        }
+    }
 
-                    if(Files.isDirectory(file)) {
+    /**
+     * Liest die Benutzericons
+     */
+    public void loadUserIcons() {
 
-                        Icon icon = new Icon();
-                        icon.setName(file.getFileName().toString());
-                        icon.setPath(file.toAbsolutePath());
-                        try (DirectoryStream<Path> stream1 = Files.newDirectoryStream(icons.resolve(file))) {
-                            for (Path iconFile : stream1) {
+        Path userIconsDirectory = Paths.get("icons");
+        if(!Files.exists(userIconsDirectory)) {
 
+            try {
+                Files.createDirectories(userIconsDirectory);
+            } catch (IOException e) {
 
+                logger.log(Level.SEVERE, "Erstellen des Ordners für die Benutzericons fehlgeschlagen", e);
+            }
+        }
+        readIconsFromDirectory(userIconsDirectory);
+    }
+
+    /**
+     * listet alle bekannten Icons auf
+     *
+     * @return Liste mit allen Icons
+     */
+    public List<Icon> getIconList() {
+
+        return iconList;
+    }
+
+    /**
+     * list die Icons aus dem übergebenen Ordner
+     *
+     * @param directory Ordner
+     */
+    protected void readIconsFromDirectory(Path directory) {
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
+            for (Path file: stream) {
+
+                if(Files.isDirectory(file)) {
+
+                    Icon icon = new Icon();
+                    icon.setName(file.getFileName().toString());
+                    icon.setPath(file.toAbsolutePath());
+                    try (DirectoryStream<Path> stream1 = Files.newDirectoryStream(directory.resolve(file), "*.{png,jpg}")) {
+                        for (Path iconFile : stream1) {
+
+                            String fileName = iconFile.getFileName().toString().toLowerCase();
+                            int size = 0;
+                            if(fileName.endsWith("png")) {
+
+                                size = Integer.parseInt(fileName.replace(".png", ""));
+                                icon.getAvailableSize().add(size);
+                            } else if(fileName.endsWith("jpg")) {
+
+                                size = Integer.parseInt(fileName.replace(".jpg", ""));
+                                icon.getAvailableSize().add(size);
                             }
-                        } catch (IOException | DirectoryIteratorException e) {
-
-                            logger.log(Level.SEVERE, "Fehler beim einlesen der Icons", e);
                         }
+                        iconList.add(icon);
+                    } catch (IOException | DirectoryIteratorException e) {
+
+                        logger.log(Level.SEVERE, "Fehler beim einlesen der Icons", e);
                     }
                 }
-            } catch (IOException | DirectoryIteratorException e) {
-
-                logger.log(Level.SEVERE, "Fehler beim einlesen der Icons", e);
             }
+        } catch (IOException | DirectoryIteratorException e) {
+
+            logger.log(Level.SEVERE, "Fehler beim einlesen der Icons", e);
+        }
+    }
+
+    /**
+     * liest icons aus der übergebenen Jar Datei
+     *
+     * @param jarFilePath Jar Datei
+     */
+    protected void readIconsFromJarFile(Path jarFilePath) {
+
+        try {
+
+            //Jar Datei einlesen
+            JarFile jarFile = new JarFile(jarFilePath.toFile());
+            Enumeration<JarEntry> entries = jarFile.entries();
+            List<JarEntry> iconEntries = new ArrayList<>();
+            while (entries.hasMoreElements()) {
+
+                JarEntry entry = entries.nextElement();
+                if(entry.getName().startsWith("icons/")) {
+
+                    iconEntries.add(entry);
+                }
+            }
+
+            //Daten auswerten
+            for (JarEntry entry : iconEntries) {
+
+                String fileName = entry.getName().toLowerCase();
+                if(!fileName.equals("icons/") && fileName.endsWith("/")) {
+
+                    String iconName = fileName.replace("icons/", "").replace("/", "");
+                    Icon icon = new Icon();
+                    icon.setName(iconName);
+                    icon.setPath(Paths.get(fileName));
+                    icon.setJarFile(true);
+                    for(JarEntry iconFile : iconEntries) {
+
+                        String iconFileName = iconFile.getName().toLowerCase();
+                        if(iconFileName.startsWith(fileName) && (iconFileName.endsWith(".png") || iconFileName.endsWith(".jpg"))) {
+                            int size = 0;
+                            if(iconFileName.endsWith(".png")) {
+
+                                //PNG Datei
+                                size = Integer.parseInt(iconFileName.replace(fileName, "").replace(".png", ""));
+                                icon.getAvailableSize().add(size);
+                            } else if(iconFileName.endsWith(".jpg")) {
+
+                                //JPG Datei
+                                size = Integer.parseInt(iconFileName.replace(fileName, "").replace(".jpg", ""));
+                                icon.getAvailableSize().add(size);
+                            }
+                        }
+                    }
+                    iconList.add(icon);
+                }
+            }
+
+            //Jar Datei schliesen
+            jarFile.close();
+        } catch (IOException e) {
+
+            logger.log(Level.SEVERE, "Fehler beim einlesen der Icons", e);
         }
     }
 }
